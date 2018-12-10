c572204d74e6c53488df03309fc55d2910d6d049
pauljeroenr <44580826+pauljeroenr@users.noreply.github.com>
2018-12-10 21:14:57 +0100

    added Comments

--- earthquake24_dashboard.R
+++ earthquake24_dashboard.R
@@ -1,11 +1,17 @@
 library("jsonlite")
 library("tidyverse")
 
+# Download of the Data as JSON
 data <- fromJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")
 
+# Row Bind 2 Dataframes of the JSON Date
 df <- bind_cols(data$features$properties, data$features$geometry)
 
+# Clean the Data
+
+# Separate Coordinates Column into Longitude Latidude and Depth
 df <- separate(df, coordinates, c("lng", "lat", "depth"), sep = ",")
+# Remove c and parenthesis from the new Columns
 df$lng <- 
   df$lng %>% 
   str_replace_all("c", "") %>% 
